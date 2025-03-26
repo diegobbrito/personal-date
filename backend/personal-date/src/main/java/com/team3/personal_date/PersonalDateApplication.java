6f9a235e6e0b983d8292cdb6a385ae16a1be5365
Diego Brito <dibbrito@gmail.com>
2025-03-25 22:20:39 -0300

    Initial commit

---
+++ backend/personal-date/src/main/java/com/team3/personal_date/PersonalDateApplication.java
@@ -0,0 +1,13 @@
+package com.team3.personal_date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDateApplication.class, args);
+	}
+
+}
