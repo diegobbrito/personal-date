b28ac242d4b9096877320d9347a446f08f1a26d0
Igor Moura <igor.oliveira.moura@gmail.com>
2025-03-25 22:43:54 -0300

    refactor: refactor "ValueCards" to use "Card" component

---
+++ frontend/src/components/ValueCards/ValueCard/Card.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface ValueCardProps {
+  title: string;
+  price: string;
+  features: string[];
+  buttonText: string;
+  buttonColor: string;
+}
+
+const ValueCard: React.FC<ValueCardProps> = ({
+  title,
+  price,
+  features,
+  buttonText,
+  buttonColor,
+}) => {
+  return (
+    <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 bg-white">
+      <h2 className="text-2xl font-bold text-center py-8">{title}</h2>
+      <p className="text-center text-4xl font-bold">{price}</p>
+      <div className="text-center font-medium flex-grow">
+        {features.map((feature, index) => (
+          <p key={index} className="py-2 border-b mx-4 mt-6">
+            {feature}
+          </p>
+        ))}
+      </div>
+      <button
+        className={`bg-${buttonColor} text-white w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 self-end`}
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default ValueCard;
