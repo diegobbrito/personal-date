b66048c9470838443fe7e351f6bc19233f149a1d
Igor Moura <igor.oliveira.moura@gmail.com>
2025-03-26 22:31:58 -0300

    feat: start of invite page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import MakeYourInvite from "./pages/MakeYourInvite";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/makeyourinvite" element={<MakeYourInvite/>}/>
       </Routes>
     </BrowserRouter>
   );
