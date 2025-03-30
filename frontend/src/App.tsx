70a8706eee92ae4b4ace1f7abc0022948c8de08c
Igor Moura <igor.oliveira.moura@gmail.com>
2025-03-30 18:11:25 -0300

    feat: creation of display invite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MakeYourInvite from "./pages/MakeYourInvite";
 import Checkout from "./pages/Checkout";
+import DisplayInvite from "./pages/DisplayInvite";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/makeyourinvite" element={<MakeYourInvite/>}/>
         <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/invite/:id" element={<DisplayInvite/>}/>
       </Routes>
     </BrowserRouter>
   );
