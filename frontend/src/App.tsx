3e2469b8a0e220bf441d9daf29d804411e6d57aa
Igor Moura <igor.oliveira.moura@gmail.com>
2025-03-29 21:09:58 -0300

    refactor(styles): migrate index.css background to home component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MakeYourInvite from "./pages/MakeYourInvite";
+import Checkout from "./pages/Checkout";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/makeyourinvite" element={<MakeYourInvite/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,10 +3,10 @@ import ValueCards from "../components/ValueCards/ValueCards";
 
 function Home() {
   return (
-    <>
+    <div className="bg-gradient-to-tl from-[#120e61] to-[#f76d02]">
       <Hero />
       <ValueCards />
-    </>
+    </div>
   );
 }
 export default Home;
