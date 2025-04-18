073607930e1aeb9e6fb3b2d9ff6fe319171198a0
Diego Brito <dibbrito@gmail.com>
2025-04-18 19:15:44 -0300

    feat: Create AdminController

---
+++ backend/personal-date/src/main/java/com/team3/personal_date/api/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.team3.personal_date.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+ } 
